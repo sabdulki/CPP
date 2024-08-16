aa4dd7a010a9bfd6a8404cb4382a8e3bd61a9eda
Sofya <abdulkina.s328@gmail.com>
2024-08-16 13:06:17 +0400

    new func

--- getStarted/example1.cpp
+++ getStarted/example1.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
 using namespace std;
 
+// am i allowed to use for loop??
+
+void PrintData();
+
 int main()
 {
 	/* cout (pronounced "see-out") is an object 
 	used together with the insertion operator (<<) 
 	to print text.*/
+	PrintData();
+	return (0);
+}
+
+void PrintData(void)
+{
 	int newNum;
 	double newDouble;
 	string newString;
 
 	newNum = 123;
 	newDouble = 1.2;
-	newString = "there are string data type! OMG!!!\n"; 
+	newString = "there is string data type! OMG!!!\n"; 
 	cout << "Hello world!\n";
 	cout << newNum << ", " << newDouble << ", " << newString;
-	return (0);
 }
