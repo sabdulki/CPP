232668be8d0ef2b4ff6c0cd605ac7b2917c722ff
Sofya Abdulkina <sabdulki@lab1r5s3.42abudhabi.ae>
2024-08-19 17:00:22 +0400

    added rules.txt

--- 00_CPP/ex00/megaphone.cpp
+++ 00_CPP/ex00/megaphone.cpp
@@ -6,33 +6,14 @@
 /*   By: sabdulki <sabdulki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 16:31:56 by sabdulki          #+#    #+#             */
-/*   Updated: 2024/08/19 16:55:28 by sabdulki         ###   ########.fr       */
+/*   Updated: 2024/08/19 16:59:48 by sabdulki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-/* 
-1) you can’t use any other external library. It means C++11 (and derived
-forms) and Boost libraries are forbidden. 
-2) friend keywords are forbidden.
-3) You should be able to use each of your headers independently from others. Thus,
-they must include all the dependencies they need. However, you must avoid the
-problem of double inclusion by adding include guards. Otherwise, your grade will
-be 0.
-*/
 
 #include <iostream>
 #include <string>
 #include <cctype>  // For std::toupper
 using namespace std;
-
-/*
-class names in UpperCamelCase format. Files containing class code will
-always be named according to the class name. For instance:
-ClassName.hpp/ClassName.h, ClassName.cpp, or ClassName.tpp. Then, if you
-have a header file containing the definition of a class "BrickWall" standing for a
-brick wall, its name will be BrickWall.hpp.
-*/
-
 
 int doUpp(string str)
 {
