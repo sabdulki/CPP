ba77e998641480e3a4435b40a3e653c31bda8682
Sofya <abdulkina.s328@gmail.com>
2024-10-28 14:47:14 +0400

    cpp08 ex00 done

--- 08_CPP/ex00/easyfind.hpp
+++ 08_CPP/ex00/easyfind.hpp
@@ -6,20 +6,41 @@
 /*   By: sabdulki <sabdulki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 18:51:58 by sabdulki          #+#    #+#             */
-/*   Updated: 2024/10/22 18:53:19 by sabdulki         ###   ########.fr       */
+/*   Updated: 2024/10/28 14:46:48 by sabdulki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <unistd.h>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <iterator>
+#include <cstdlib>
+#include <ctime>
 
-
-class EasyFind
+/* Using T::iterator ensures it will be compatible with 
+the container type and gives us a uniform way 
+to access elements within it.*/
+template <typename T>
+typename T::iterator easyfind(T& container, int element)
 {
+	typename T::iterator it = std::find(container.begin(), container.end(), element);
+	//std::find is an algorithm from the Standard Template Library (STL) 
+	// container.begin(): An iterator pointing to the first element in container.
+	// container.end(): An iterator pointing just past the last element of container.
+	//returns an iterator to the found element
+	// it = std::find(container.begin(), container.end(), element);
 	
-};
-
-#include "easyfind.tpp"
+	/*std::find goes through each element from container.begin() to container.end() and 
+	checks if it equals value. It stops searching as soon as it finds a match 
+	or reaches container.end().*/
+	if (it == container.end())
+		throw(std::out_of_range("element is not found"));
+	return (it);
+}
 
 #endif
--- 08_CPP/ex00/main.cpp
+++ 08_CPP/ex00/main.cpp
@@ -6,13 +6,50 @@
 /*   By: sabdulki <sabdulki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 18:50:50 by sabdulki          #+#    #+#             */
-/*   Updated: 2024/10/22 18:54:13 by sabdulki         ###   ########.fr       */
+/*   Updated: 2024/10/28 14:46:29 by sabdulki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
 
+void test1()
+{
+	std::vector<int> containerV;
+	for (int i = 0; i < 10; i++)
+		containerV.push_back(i);
+	try
+	{
+		std::cout << "vector elemet: " << *(easyfind(containerV, 1)) << std::endl;
+	}
+	catch(std::out_of_range& e) {
+		std::cout << "Exception occured: " << e.what() << std::endl;
+	}
+}
+
+void test2()
+{
+	std::list<int> containerL;
+	int random;
+
+	for(int i = 0; i < 10; i++)
+	{
+		std::srand(static_cast<unsigned>(time(0)));
+		random = rand() % 20;
+		std::cout << random << std::endl;
+		containerL.push_front(random);
+		sleep(1);
+	}
+	try
+	{
+		std::cout << "list elemet: " << *(easyfind(containerL, 3)) << std::endl;
+	}
+	catch(std::out_of_range& e) {
+		std::cout << "Exception occured: " << e.what() << std::endl;
+	}
+}
+
 int main()
 {
-	
+	test1();
+	test2();
 }
