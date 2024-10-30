1273b93af9cb82d5b8a6f7f21a3681e5df96fe7f
Sofya <abdulkina.s328@gmail.com>
2024-10-30 17:41:22 +0400

    cpp08 ex02 done. all exs done

--- 08_CPP/ex02/MutantStack.hpp
+++ 08_CPP/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: sabdulki <sabdulki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 12:13:32 by sabdulki          #+#    #+#             */
-/*   Updated: 2024/10/30 13:08:05 by sabdulki         ###   ########.fr       */
+/*   Updated: 2024/10/30 17:37:46 by sabdulki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,38 @@
 //You can use any container type that supports 
 // the same interface as std::deque (such as std::vector or std::list
 template <typename T, typename anyContainer =  std::deque<T> >
-class MutantStack : public std::stack<T, Container> // using Container as the underlying storage.
+class MutantStack : public std::stack<T, anyContainer> // using Container as the underlying storage.
 {
 	public:
 		MutantStack();
 		~MutantStack();	
-		typedef typename anyContainer::iterator = iterator;
-		typedef typename anyContainer::const_iterator = const_iterator;
+		typedef typename anyContainer::iterator iterator;
+		typedef typename anyContainer::const_iterator const_iterator;
 
-		iterator begin();
-		iterator end();
-		const_iterator begin() const;
-		const_iterator end() const;
+		iterator begin() {
+			return this->c.begin();
+		}
+		iterator end(){
+			return this->c.end();
+		}
+		const_iterator begin() const{
+			return this->c.begin();
+		}
+		const_iterator end() const{
+			return this->c.end();
+		}
 	
 };
+
+
+
+//there is a hidden c member (of type std::deque<int>) that stores the actual elements.
+/*In your example, since you’re using std::stack<int>, the default underlying container 
+is std::deque<int>, unless you specified a different container type.*/
+/*
+Since this->c is an instance of std::deque<int>, you can access all 
+the public member functions of std::deque, such as push_back(), pop_back(), begin(), end(), etc.*/
+
 
 #include "MutantStack.tpp"
 
